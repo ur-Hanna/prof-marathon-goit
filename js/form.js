3381fe4718328f481e2b0c048f9421d940c1c132
Hanna Rosytiuk <hanna.mainmail@gmail.com>
2024-01-10 18:52:20 +0200

    bugfix/fix js

--- js/form.js
+++ js/form.js
@@ -9,35 +9,39 @@ goToFormButton.addEventListener('click', function (e) {
 });
 
 function clearFormFields() {
-  const modalFiends = formModal.querySelectorAll('input');
+  const fieldName = form.querySelector('input[type="text"]');
+  const fieldEmail = form.querySelector('input[type="email"]');
 
-  modalFiends.forEach(field => {
-    field.value = '';
-  });
+  fieldName.value = '';
+  fieldEmail.value = '';
+}
+
+function addGooseElement() {
+  const targetContainer = document.querySelector('#form');
+  const gooseEl = document.createElement('img');
+  gooseEl.classList.add('gus-anim');
+
+  targetContainer.appendChild(gooseEl);
 }
 
 function showGooseAnim() {
-  const targetContainer = document.querySelector('#form');
-  const gusImage = document.createElement('img');
-  gusImage.setAttribute('src', './images/gus-anim.gif');
-  gusImage.classList.add('gus-anim');
+  const gooseEl = document.querySelector('.gus-anim');
 
-  targetContainer.appendChild(gusImage);
+  gooseEl.setAttribute('src', './images/gus-anim.gif');
 
-  setTimeout(2000, () => {
-    targetContainer.removeChild(gusImage);
-  });
+  setTimeout(() => {
+    gooseEl.removeAttribute('src');
+  }, 4000);
 }
+
+addGooseElement();
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   const formData = new FormData(form);
 
   launchBtn.setAttribute('disabled', true);
-
-  if (userEmailField?.value?.length > 30) {
-    return;
-  }
+  launchBtn.style.opacity = '0.7';
 
   fetch('/', {
     method: 'POST',
@@ -47,10 +51,12 @@ form.addEventListener('submit', e => {
     .then(() => {
       showGooseAnim();
 
+      launchBtn.removeAttribute('disabled');
+      clearFormFields();
+
       setTimeout(() => {
-        // launchBtn.removeAttribute('disabled')
-        // clearFormFields();
-      }, 2000);
+        launchBtn.style.opacity = '1';
+      }, 4000);
     })
     .catch(error => console.log('Sending form failed'));
 });
